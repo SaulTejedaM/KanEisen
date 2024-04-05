02e00beaa81086a668ee96c3d75fdc12862af414
SaulTejedaM <saultejm@gmail.com>
2024-04-04 21:21:44 -0600

    Add files via upload

--- kaneisen/src/components.js
+++ kaneisen/src/components.js
@@ -3,19 +3,15 @@ import { FaPlus, FaCog, FaCircle } from 'react-icons/fa';
 import { MdDashboardCustomize } from "react-icons/md";
 import { IoClose } from "react-icons/io5";
 // MdDashboard
-export function ModalSettings({ isOpen, onClose }) {
-    if (!isOpen) return null;
 
+export function ModalSettings({ isOpen, onClose }) {
     return (
-        <div class="modal-background">
-            <div class="modal-content">
-                <div class='modal-title-section'>
-                    <h1>Settings</h1>
-                    <span class="close-modal" onClick={onClose}>
-                        <IoClose />
-                    </span>
-                </div>
-                
+        <div className="modal" style={{ display: isOpen ? 'block' : 'none' }}>
+            <div className="modal-content">
+                <span className="close-modal" onClick={onClose}>
+                    <IoClose />
+                </span>
+                <h1>Settings</h1>
             </div>
         </div>
     );
@@ -24,43 +20,37 @@ export function ModalSettings({ isOpen, onClose }) {
 
 
 export function ActionMenu() {
-    const [isSettingsOpen, setIsSettingsOpen] = useState(false);
 
-    const openSettings = () => {
-        setIsSettingsOpen(true);
+    const [modalOpen, setModalOpen] = useState(false);
+
+    const openModal = () => {
+        setModalOpen(true);
     };
 
-    const closeSettings = () => {
-        setIsSettingsOpen(false);
+    const closeModal = () => {
+        setModalOpen(false);
     };
 
     return (
         <section id="titleSection">
             <div id="action-menu">
-                <button class="action-button">
+                <button className="action-button">
                     <FaPlus />
                 </button>
-                <button class="action-button">
+                <button className="action-button">
                     <MdDashboardCustomize />
                 </button>
-                <button class="action-button" onClick={openSettings}>
+                <button id="myBtn" className="action-button" onClick={openModal}>
                     <FaCog />
                 </button>
             </div>
-            <ModalSettings isOpen={isSettingsOpen} onClose={closeSettings} />
-            {/* <div id="title">
-                <MdDashboard/>
-                <h1>
-                    KanEisen
-                </h1>
-            </div> */}
+            <ModalSettings isOpen={modalOpen} onClose={closeModal} />
         </section>
     );
 }
 
-
-
 export function TaskStatusSection(){
+
     return (
         <section id="taskStatusSection">
             <div class="listSpace">
@@ -84,5 +74,3 @@ export function TaskStatusSection(){
         </section>
       );
 }
-
-
