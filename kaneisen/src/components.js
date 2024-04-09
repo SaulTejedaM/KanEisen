1b596471ee8e35088d6948ecfc14f7d3c6024726
SaulTejedaM <saultejm@gmail.com>
2024-04-08 23:32:52 -0600

    Add files via upload

--- kaneisen/src/components.js
+++ kaneisen/src/components.js
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
-import { FaPlus, FaCog, FaCircle, FaRegQuestionCircle, FaKeyboard } from 'react-icons/fa';
+import { FaPlus, FaCog, FaCircle, FaRegQuestionCircle } from 'react-icons/fa';
 import { MdDashboardCustomize } from "react-icons/md";
-import { RiFontSize } from "react-icons/ri";
+// import { RiFontSize } from "react-icons/ri";
 import { IoClose, IoInvertModeOutline, IoLanguage } from "react-icons/io5";
+// FaKeyboard
 // MdDashboard
 // MdMode edit icon
 
@@ -11,51 +12,65 @@ export function ModalSettings({ isOpen, onClose }) {
         <div class="modal" style={{ display: isOpen ? 'block' : 'none' }}>
             <div class="modal-content">
                 <div class="modal-title">
-                    <h1>Settings</h1>
+                    <h1>
+                        Settings
+                    </h1>
                     <span class="close-modal" onClick={onClose}>
                         <IoClose />
                     </span>
                 </div>
-                <button>
-                    <IoInvertModeOutline />
-                    <p class="modal-subtitle">Theme</p>
-                    <p class="modal-text">
-                        Customize visual style of the interface.
-                    </p>
-                </button>
                 <div class="modal-section">
-                    <button>
+                    <button class="settings-option">
+                        <IoInvertModeOutline />
+                        <p class="modal-subtitle">
+                            Theme
+                        </p>
+                        <p class="modal-text">
+                            Customize visual style of the interface.
+                        </p>
+                    </button>
+                </div>
+                {/* <div class="modal-section">
+                    <button class="settings-option">
                         <RiFontSize />
-                        <p class="modal-subtitle">Font-Size</p>
+                        <p class="modal-subtitle">
+                            Font-Size
+                        </p>
                         <p class="modal-text">
                             Adjust the text size for better readability and visual appeal.
                         </p>
                     </button>
-                </div>
-                <div class="modal-section">
-                    <button>
+                </div> */}
+                {/* <div class="modal-section">
+                    <button class="settings-option">
                         <FaKeyboard />
-                        <p class="modal-subtitle">Shortcuts</p>
+                        <p class="modal-subtitle">
+                            Shortcuts
+                        </p>
                         <p class="modal-text">
                             Customize and define their preferred keyboard shortcuts for various actions.
                         </p>
                     </button>
-                </div>
+                </div> */}
                 <div class="modal-section">
-                    <button>
+                    <button class="settings-option">
                         <IoLanguage />
-                        <p class="modal-subtitle">Language</p>
+                        <p class="modal-subtitle">
+                            Language
+                        </p>
                         <p class="modal-text">
                             Choose your preferred language for the user interface.
                         </p>
                     </button>
                 </div>
                 <div class="modal-section">
-                    <button>
+                    <button class="settings-option">
                         <FaRegQuestionCircle />
-                        <p class="modal-subtitle">About</p>
+                        <p class="modal-subtitle">
+                            About
+                        </p>
                         <p class="modal-text">
-                            Information about the application, including its version number, development team, and any relevant information.
+                            Relevant information about the application.
                         </p>
                     </button>
                 </div>
@@ -69,22 +84,55 @@ export function ModalNewTask({ isOpen, onClose }) {
         <div class="modal" style={{ display: isOpen ? 'block' : 'none' }}>
             <div class="modal-content">
                 <div class="modal-title">
-                    <h1>New Task</h1>
+                    <input id="task-name" defaultValue={"Untitled"}/>
                     <span class="close-modal" onClick={onClose}>
                         <IoClose />
                     </span> 
                 </div>
                 <div class="modal-section">
-                    <div class="modal-title">
-                        <p class="modal-subtitle">Status: </p>
-                        <p class="modal-subtitle">Category: </p>
+                    <div id="task-status" class="modal-title">
+                        <p class="modal-subtitle">
+                            Status 
+                        </p>
+                        <button id="activity-status" class="modal-task-status">
+                            <FaCircle />
+                            <p>
+                                Delegate
+                            </p>
+                        </button>
+                        <p class="modal-subtitle">
+                            Category 
+                        </p>
+                        <button id="activity-status" class="modal-task-status">
+                            <FaCircle />
+                            <p>
+                                Delegate
+                            </p>
+                        </button>
                     </div>
                 </div>
                 <div class="modal-section">
-                    <p class="modal-subtitle">Desciption: </p>
+                    <p class="modal-subtitle">
+                        Desciption: 
+                    </p>
+
+                </div>
+                <div class="modal-section">
+                    <p  id="task-description" class="modal-text">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt 
+                        ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco 
+                        laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in 
+                        voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat 
+                        non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    </p>
                 </div>
                 <div class="button-section">
-                    <button class="save-button">Save</button>
+                    <button class="save-button">
+                        Save
+                    </button>
+                    {/* <button class="delete-button">
+                        Delete
+                    </button> */}
                 </div>
             </div>
         </div>
@@ -96,7 +144,9 @@ export function ModalMatrix({ isOpen, onClose }) {
         <div class="modal" style={{ display: isOpen ? 'block' : 'none' }}>
             <div class="modal-content">
                 <div class="modal-title">
-                    <h1>Eisenhower Matrix</h1>
+                    <h1>
+                        Eisenhower Matrix
+                    </h1>
                     <span class="close-modal" onClick={onClose}>
                         <IoClose />
                     </span> 
@@ -105,7 +155,6 @@ export function ModalMatrix({ isOpen, onClose }) {
         </div>
     );
 }
-
 
 export function ActionMenu() {
     const [newTaskModalOpen, setNewTaskModalOpen] = useState(false);
@@ -137,7 +186,7 @@ export function ActionMenu() {
     };
 
     return (
-        <section id="titleSection">
+        <section id="header">
             <div id="action-menu">
                 <button class="action-button" onClick={openNewTaskModal}>
                     <FaPlus />
@@ -162,19 +211,25 @@ export function TaskStatusSection(){
             <div class="listSpace">
                 <div class="task-status-title" id="not-started-title">
                     <FaCircle />
-                    <p>Not Started</p>
+                    <p>
+                        Not Started
+                    </p>
                 </div>
             </div>
             <div class="listSpace">
                 <div class="task-status-title" id="in-progress-title">
                     <FaCircle />
-                    <p>In Progress</p>
+                    <p>
+                        In Progress
+                    </p>
                 </div>
             </div>
             <div class="listSpace">
                 <div class="task-status-title" id="done-title">
                     <FaCircle />
-                    <p>Done</p>
+                    <p>
+                        Done
+                    </p>
                 </div>
             </div>
         </section>
